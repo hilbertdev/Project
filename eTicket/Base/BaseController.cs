a218131e41f13d2c3e79094a996c5c3cf7d710e0
hilbertmuna <hilbertmuna@gmail.com>
2023-12-11 15:02:35 +0200

    Add BaseController and EventsController

---
+++ eTicket/Base/BaseController.cs
@@ -0,0 +1,12 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTicket.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseController : ControllerBase
+    {
+        // Common functionality or properties for all REST API controllers can be added here
+    }
+}
---
+++ eTicket/Controllers/EventsController.cs
@@ -0,0 +1,32 @@
+
+using Application.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Project.Domain.Models;
+
+namespace eTicket.Controllers
+{
+    public class EventsController : BaseController
+    {
+        private readonly IRequestHandler<GetEventQuery, Event> _getEventQueryHandler;
+        public EventsController(IRequestHandler<GetEventQuery, Event> getEventQueryHandler)
+        {
+            _getEventQueryHandler = getEventQueryHandler;
+        }
+
+        [HttpGet]
+        [Route("GetEventById")]
+        public IActionResult GetEvents([FromQuery] GetEventQuery query) 
+        {
+            var result = _getEventQueryHandler.Handle(query, new CancellationToken());
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("GetAllEvents")]
+        public IActionResult GetAllEvents()
+        {
+            return Ok();
+        }
+    }
+}
