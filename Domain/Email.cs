7ff5e3fed7dc8685d0fbbf990b6de6ed3e7991d4
hilbertmuna <hilbertmuna@gmail.com>
2023-12-25 13:01:33 +0200

    added new editor config file for project

--- Domain/Email.cs
+++ Domain/Email.cs
@@ -1,12 +1,12 @@
 
 namespace Project.Domain.Primitives.ValueObjects;
-    public sealed class Email : ValueObject
-    {
-        public string Address { get; }
+public sealed class Email : ValueObject
+{
+    public string Address { get; }
 
     public Email()
     {
-        
+
     }
 
     public Email(string address)
@@ -16,11 +16,11 @@ namespace Project.Domain.Primitives.ValueObjects;
             throw new ArgumentException("Email address cannot be empty or whitespace.", nameof(address));
         }
 
-        Address = address;
+        this.Address = address;
     }
 
     public override IEnumerable<object> GetAtomicValues()
     {
-        yield return Address.ToLowerInvariant();
+        yield return this.Address.ToLowerInvariant();
     }
 }
